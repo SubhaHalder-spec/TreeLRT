515ecfb620f6143186e1f19a8d42f4ab55e00070
Subha Halder <133703969+SubhaHalder-spec@users.noreply.github.com>
2024-05-20 22:25:28 +0530

    Update TreeLRT.R

--- R/TreeLRT.R
+++ R/TreeLRT.R
@@ -100,7 +100,7 @@ TreeLRT <- function(sample_data, significance_level){
   }
 
   sample_data <- lapply(sample_data, function(x) x[!is.na(x)])
-  num_samples = 10000
+  num_samples = 20000
   num_datasets <- length(sample_data)
   n <- sapply(sample_data, length)
   lambda_values_star <- numeric(num_samples)
